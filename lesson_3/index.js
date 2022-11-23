fa86a7dafba56e8043a76fbbb349d4837cf27216
shamma <shammacode@gmail.com>
2022-11-23 20:44:49 +0300

    refactor:lesson_3 task

--- lesson_3/index.js
+++ lesson_3/index.js
@@ -9,16 +9,50 @@ const input = fs.createReadStream(logDir, {
 
 const rl = readLine.createInterface(input);
 
-rl.on("line", line => {
-    const [filename, fileContent] = line.split(" - - ");
-    if (filename && fileContent) {
-        const logPath = path.join(__dirname, 'logs', filename + '_requests.log')
-        const output = fs.createWriteStream(logPath, {
-            encoding: "utf8",
-            flags: "a+"
-        });
-        output.write(fileContent + "\n");
-    }
-});
-rl.on("close",()=> console.log("Parse done!"))
+const destPath = path.join(__dirname, 'logs')
 
+function createLogs(cb){
+    fs.mkdir(destPath, {recursive: true}, cb);
+}
+
+function parseLogs() {
+    rl.on("line", line => {
+        const [filename, fileContent] = line.split(" - - ");
+        if (filename && fileContent) {
+            const logPath = path.join(__dirname, 'logs', filename + '_requests.log')
+            const output = fs.createWriteStream(logPath, {
+                encoding: "utf8",
+                flags: "a+"
+            });
+            output.write(fileContent + "\n");
+        }
+    });
+    rl.on("close", () => console.log("Parse done!"))
+}
+
+createLogs(parseLogs);
+
+/*На всякий случай прям совсем задание
+
+function createLogs(cb){
+    fs.mkdir(destPath, {recursive: true}, cb);
+}
+
+function saveLogs() {
+    rl.on("line", line => {
+        const [filename, fileContent] = line.split(" - - ");
+        if (filename==='89.123.1.41'|| filename==='34.48.240.111') {
+            const logPath = path.join(__dirname, 'logs', filename + '_requests.log')
+            const output = fs.createWriteStream(logPath, {
+                encoding: "utf8",
+                flags: "a+"
+            });
+            output.write(fileContent + "\n");
+        }
+    });
+    rl.on("close", () => console.log("Parse done!"))
+}
+
+createLogs(saveLogs);
+
+*/
